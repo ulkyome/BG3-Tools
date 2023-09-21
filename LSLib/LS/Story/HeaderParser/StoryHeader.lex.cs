7fd1e044caae3899fa0167ab8144cdbdcb0d2382
Ulkyome <ulkyome@hotmail.com>
2023-09-22 02:17:04 +0300

    Fix

--- LSLib/LS/Story/HeaderParser/StoryHeader.lex.cs
+++ LSLib/LS/Story/HeaderParser/StoryHeader.lex.cs
@@ -6,7 +6,7 @@
 //
 //  GPLEX Version:  1.2.2
 //  Machine:  ULKYOME-PC
-//  DateTime: 22.09.2023 02:12:46
+//  DateTime: 22.09.2023 02:13:41
 //  UserName: Ulkyome
 //  GPLEX input file <E:\source\repos\BG3_Tran\LSLib\\LS\Story\HeaderParser\StoryHeader.lex - 18.08.2023 04:17:07>
 //  GPLEX frame file <embedded resource>
